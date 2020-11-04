23286eaa3e9ecb01fd5e32ae826ecac04e33f8c4
anoxinc <pythonexception@gmail.com>
2020-11-04 21:47:45 +0400

    Assets added, for example bot images

--- mate/meta/info/bot.py
+++ mate/meta/info/bot.py
@@ -22,7 +22,7 @@ class BotInfoMeta:
 
     EMBED_TITLE = "About"
 
-    EMBED_COLOR = 0x00ff00
+    EMBED_COLOR = 0x43b581
 
     EMBED_THUMBNAIL = "https://cdn.discordapp.com/app-icons/" + \
         "705380653855932416/761155d9b1d6ddd104fe0ce82465bda0.png?size=64"
